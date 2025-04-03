244996d666ef4d7240140dc049c7440653158d43
azamatnassir <55829216+azamatnassir@users.noreply.github.com>
2025-04-03 06:31:32 +0000

    version 1.2

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import sqlite3, json
+import sqlite3
+import json
 
 app = Flask(__name__, static_folder="public", static_url_path="/")
 CORS(app)
+
 
 def init_db():
     conn = sqlite3.connect("questions.db")
@@ -32,11 +34,13 @@ def init_db():
     conn.commit()
     conn.close()
 
+
 init_db()
+
 
 @app.route("/update-question", methods=["POST"])
 def update_question():
-    data = request.json  
+    data = request.json
     question_id = data.get("id")
 
     if question_id is None:
@@ -45,48 +49,48 @@ def update_question():
     try:
         conn = sqlite3.connect("questions.db")
         cursor = conn.cursor()
-        
-        cursor.execute("UPDATE quiz_questions SET solved = 1 WHERE id = ?", (question_id,))
+
+        cursor.execute("UPDATE quiz_questions SET solved = 1 WHERE id = ?",
+                       (question_id, ))
         conn.commit()
         conn.close()
 
         return jsonify({"success": True})
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
-    
+
 
 @app.route("/update-group", methods=["POST"])
 def update_group():
-    data = request.json  
+    data = request.json
     group_name = data.get("name")
     level_name = data.get("level")
 
     if level_name is None:
         return jsonify({"success": False, "error": "Нет level_name"}), 400
-    
+
     if group_name is None:
         return jsonify({"success": False, "error": "Нет group_name"}), 400
-    
 
     if level_name == "level1":
         query = "UPDATE groups SET ironMan = ironMan + 1, total = total + 1 WHERE name = ?"
     elif level_name == "level2":
         query = "UPDATE groups SET spiderMan = spiderMan + 2, total = total + 2 WHERE name = ?"
-    else: 
+    else:
         query = "UPDATE groups SET drStrange = drStrange + 3, total = total + 3 WHERE name = ?"
 
     try:
         conn = sqlite3.connect("questions.db")
         cursor = conn.cursor()
-        
-        cursor.execute(query, (group_name,))
+
+        cursor.execute(query, (group_name, ))
         conn.commit()
         conn.close()
 
         return jsonify({"success": True})
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
-    
+
 
 @app.route("/get-questions", methods=["GET"])
 def get_questions():
@@ -110,6 +114,7 @@ def get_questions():
 
     return jsonify(data)
 
+
 @app.route("/get_groups")
 def get_groups():
     conn = sqlite3.connect("questions.db")
@@ -122,13 +127,18 @@ def get_groups():
     groups = [dict(zip(columns, row)) for row in rows]
     return jsonify(groups)
 
+
 @app.route("/leaderboard")
 def leaderboard():
     return app.send_static_file("leaderboard.html")
+
 
 @app.route("/")
 def home():
     return app.send_static_file("index.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run(debug=True, port=5000)
